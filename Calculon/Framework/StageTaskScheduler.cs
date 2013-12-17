283b027d544933570e61182f0a83616235bbba74
Arne Claassen <arne@claassen.net>
2013-12-16 22:01:59 -0800

    a fresh start

---
+++ Calculon/Framework/StageTaskScheduler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Droog.Calculon.Framework {
+    public class StageTaskScheduler : TaskScheduler {
+        private readonly ConcurrentQueue<Task> _queue = new ConcurrentQueue<Task>();
+ 
+        protected override void QueueTask(Task task) {
+            Console.WriteLine("queueing task");
+            //_queue.Enqueue(task);
+            TryExecuteTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+            return false;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks() {
+            return _queue.ToArray();
+        }
+    }
+}
--- Calculon/Framework/Transport.cs
+++ Calculon/Framework/Transport.cs
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 namespace Droog.Calculon.Framework {
     public class Transport : ITransport {
         private readonly ActorAddress _address;
